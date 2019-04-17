8e694c2212df80de4180e380b70d0fe55ad253ee
viktoria-shp <vvitashp@gmail.com>
2019-04-17 18:39:36 +0300

    kappe

--- app/js/src.js
+++ app/js/src.js
@@ -27,8 +27,7 @@ function filterByHash(hash) {
   $('.filter a').parent().removeClass('is-active')
   $('.filter a[href="#' + hash + '"]').parent().addClass('is-active')
 }
-$('.my-scrollbar').each(function(){
-})
+
 Scrollbar.init($('.my-scrollbar').get(0), {
   damping: 0.2
 });
@@ -53,3 +52,90 @@ content.addListener(function(status) {
     $('.scroll-up').addClass('hide')
   }
 });
+
+var $grid = $('.grid').imagesLoaded( function() {
+  $grid.masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer',
+    gutter: 0
+  }); 
+});
+
+function renderPosts(posts){
+  console.log(posts);
+  if (posts != undefined) {
+    posts.forEach( function(item) {
+
+      let post = '<div class="grid-item ' + item.type + '">'
+      switch (item.type) {
+        case 'img':
+          post += '<img src="' + item.img + '">'
+          break;
+        case 'quote':
+          post += '<blockquote class="quote__text">' + item.hreff + '</blockquote><div class="quote__autor">' + item.author + '</div>'
+          break;
+        case 'link':
+          post += '<div class="link__title">' + item.title + '</div>'+'<div class="link__hreff">'+'<a href="' + item.hreff + '">'+item.hrefftext+'</a></div>'
+          break;
+        case 'aside':
+          post += '<div class="aside__title">' + item.title + '</div>'+'<div class="aside__text">' + item.text + '</div>'
+          break;
+        case 'standard':
+          post += '<div class="standard__img">'+'<img src="' + item.img + '">'+'</div>'+'<div class="standard__title">' + item.title + '</div>'+'<div class="standard__text">' + item.text + '</div>'+'<div class="blog-icons">' + '<div class="blog-icons__comment">' + '<i class="fas fa-comment"></i>' + item.comment + '</i>'+'</div>' + '<div class="blog-icons__comment">' + '<i class="fas fa-calendar-alt"></i>' + item.calendar + '</div>'+'</div>'
+          break;
+        case 'video':
+          post += '<div class="video__img">'+'<iframe width="294" height="381" src="https://www.youtube.com/embed/YNY_pb4b22k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>'+'</iframe>'+'</div>'+'<div class="video__title">' + item.title + '</div>'+'<div class="video__text">' + item.text + '</div>'+'<div class="blog-icons">' + '<div class="blog-icons__comment">' + '<i class="fas fa-comment"></i>' + item.comment + '</i>'+'</div>' + '<div class="blog-icons__comment">' + '<i class="fas fa-calendar-alt"></i>' + item.calendar + '</div>'+'</div>'
+          break;
+        case 'gallery':
+          post += '<div class="w3-content w3-display-container">'
+          item.imgs.forEach( function(img, index) {
+              post += '<img class="mySlides" src="' + img + '" ' + (index==0 ? 'style="display: block;"' : '')+ '>'
+          });
+          post += '<button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, this)"><i class="fas fa-chevron-left"></i></button>'
+          post += '<button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, this)"><i class="fas fa-chevron-right"></i></button>'
+          post += '<div class="gallery__title">' + item.title + '</div>'+'<div class="gallery__text">' + item.text + '</div>'+'<div class="blog-icons">' + '<div class="blog-icons__comment">' + '<i class="fas fa-comment"></i>' + item.comment + '</i>'+'</div>' + '<div class="blog-icons__comment">' + '<i class="fas fa-calendar-alt"></i>' + item.calendar + '</div>'+'</div>'
+          post += '</div>'
+          break;
+        default:
+          break;
+      }
+      post += '</div>'
+      $('.grid').append(post)
+    });
+    $('.grid').imagesLoaded( function() {        
+        $grid.masonry().masonry('reloadItems')        
+    })    
+  }
+}
+function getPosts(page){
+  let posts = []
+  fetch('http://localhost:3000/?page='+page)
+    .then(function(response) {
+      //alert(response.headers.get('Content-Type')); // application/json; charset=utf-8
+      //alert(response.status); // 200
+
+    return response.json();
+   })
+  .then(function(posts) {
+    console.log(posts)
+    renderPosts(posts)
+  })
+  .catch(alert);
+  return(posts)
+}
+//getPosts()
+
+
+$(document).ready(function() {
+    $('.sidebar-opener').click(function() {
+        $('.sidebar').toggleClass('open');
+    });
+    $('.older-post').click(function() {
+      var page = parseInt($(this).data('page'));
+      getPosts(page)        
+      $(this).data('page', (page+1));
+    });
+});
+
+
--- app/js/src1.js
+++ app/js/src1.js
@@ -8,3 +8,93 @@ document.addEventListener('DOMContentLoaded', function(){
     })
   }
 });
+var slider = document.querySelector('.slider');
+  if(slider!=undefined){
+    var hammertime = new Hammer(slider);
+    var slides = document.querySelectorAll('.slide');
+    function goToSlide(index){
+      [].forEach.call(slides, function(item){
+        item.classList.remove('is-active')
+        item.classList.remove('is-next')
+        item.classList.remove('is-prev')
+      });
+      if(!slides[index]){
+        return false
+      }
+      if (index+1 >= slides.length){
+        slides[0].classList.add('is-next')
+      }else{
+        slides[index+1].classList.add('is-next')
+      }
+      if (index-1 < 0){
+        slides[slides.length-1].classList.add('is-prev')
+      }else{
+        slides[index-1].classList.add('is-prev')
+      }
+      slides[index].classList.add('is-active')
+      slider.style.height = slides[index].clientHeight+'px'
+      let allPins = document.querySelectorAll('.slider-pins div');
+      [].forEach.call(allPins, function(item){
+        item.classList.remove('active');
+      });
+      allPins[index].classList.add('active');
+    }
+    function sliderInit(){
+      var num = 0;
+      var pins = document.createElement('div');
+      pins.classList.add('slider-pins');
+      [].forEach.call(slides, function(item){
+        var pin = document.createElement('div');
+        pin.innerText = num;
+        pin.dataset.num = num;
+        num++;
+        pin.addEventListener('click', function(){
+          console.log(this.innerText, this.dataset.num)
+          goToSlide(+this.innerText);
+        })
+        pins.appendChild(pin)
+      })
+      slider.appendChild(pins)
+      goToSlide(0);
+    }
+    sliderInit();
+    hammertime.on('swipeleft', function(ev) {
+      let current = document.querySelector('.slide.is-active');
+      var index = Array.prototype.indexOf.call( slides,current);
+      index++;
+      if (index >= slides.length){
+        index = 0;
+      }
+      goToSlide(index);
+    });
+    hammertime.on('swiperight', function(ev) {
+      let current = document.querySelector('.slide.is-active');
+      var index = Array.prototype.indexOf.call( slides,current);
+      index--;
+      if (index < 0){
+        index = slides.length-1;
+      }
+      goToSlide(index);
+    });
+    hammertime.on('swiperight', function(ev) {
+    });
+  }
+
+var slideIndex = 1;
+function plusDivs(n, element) {
+  showDivs(slideIndex += n, element);
+
+}
+function showDivs(n, element) {
+  var i;
+  var x = element.parentElement.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  x[slideIndex-1].style.display = "block"; 
+  console.log(x)
+}
+
+
